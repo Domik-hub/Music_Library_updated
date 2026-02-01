867b7ea890f8eccbdda4eb5caba6a0bf0b41e823
Domik-hub <dusenbai0064@gmail.com>
2026-02-01 20:48:54 +0500

    schema sql

--- resources/schema.sql
+++ resources/schema.sql
@@ -1,10 +1,21 @@
+CREATE TABLE playlists (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
 CREATE TABLE media (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    type VARCHAR(50) NOT NULL
+    artist VARCHAR(100),
+    type VARCHAR(50) NOT NULL,
+    playlist_id INTEGER,
+    CONSTRAINT fk_playlist
+        FOREIGN KEY(playlist_id)
+        REFERENCES playlists(id)
+        ON DELETE SET NULL
 );
-
-INSERT INTO media (title, type) VALUES
-('In This Darkness - Clara La San', 'SONG'),
-('Want You - Clara La San', 'SONG'),
-('My Love Mine All Mine - Mitski', 'SONG');
+INSERT INTO media (title, artist, type) VALUES
+    ('In This Darkness', 'Clara La San', 'SONG'),
+    ('Want You', 'Clara La San', 'SONG'),
+    ('My Love Mine All Mine', 'Mitski', 'SONG'),
+    ('sadisfaction', 'королевский XVII', 'SONG');
